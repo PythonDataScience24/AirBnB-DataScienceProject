077f71eb1f203eb9cbdaab5e6267c1ca515dcba2
Wi42 <101128608+Wii42@users.noreply.github.com>
2024-04-28 14:16:05 +0200

    show locations on streamlit map

--- src/hellostreamlit.py
+++ src/hellostreamlit.py
@@ -1,12 +1,13 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
-#we're testing whether we can display data
+# we're testing whether we can display data
 
 st.title("Figure out if we want to use streamlit")
 
-st.map()
 df = pd.read_csv('data/Airbnb_Open_Data.csv')
-df_10 = df.head(10)
-st.table(data=df_10)
+df_10 = df.head(10).drop(columns=['house_rules'])
+df_map = df.dropna(subset=['lat', 'long'])
 
+st.map(data=df_map, latitude='lat', longitude='long', size=1)
+st.table(data=df_10)
