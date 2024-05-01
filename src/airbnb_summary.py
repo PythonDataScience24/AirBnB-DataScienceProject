9f3d526eb49d0e393595393769226a981957a72c
Viola Meier <viola.camille.meier@gmail.com>
2024-05-01 15:55:59 +0200

    added airbnb_summary.py

---
+++ src/airbnb_summary.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+
+class AirBnBSummary: 
+    
+    def __init__(self, csv_path: str = None, df: pd.DataFrame = None):
+        assert csv_path is not None or df is not None, "Either csv_path or df must be provided"
+        if df is not None:
+            self.df = df
+        else:
+            self.df = pd.read_csv(csv_path)
+            self.clean_data()
+    
+    def clean_data(self)-> pd.DatatFrame: 
+        self.df.drop_duplicates(subset=['id'])
+        self.df.dropna(subset = ['id'])
+        self.df.loc[self.df["neighbourhood group"] == "manhatan", "neighbourhood group"] = "Manhattan"
+        self.df.loc[self.df["neighbourhood group"] == "brookln", "neighbourhood group"] = "Brooklyn"
+    
+    def get_total_airbnbs(self) -> pd.DataFrame:
+        return self.df.shape[0]
+    
+    def get_airbnbs_per_nhood(self) -> pd.DataFrame:
+        airbnbs_per_group = self.df.groupby('neighbourhood group').size()
+        return airbnbs_per_group
