ba91f8048d42a1f02bed234a09b43d5eb8492f23
Viola Meier <viola.camille.meier@gmail.com>
2024-05-14 16:19:17 +0200

    refactored rating.py

--- src/rating.py
+++ src/rating.py
@@ -1,9 +1,26 @@
 import pandas as pd
 
-
 class RatingSummary:
+    
+    """
+        Calculates and summarize rating statistics for Airbnb listings.
+
+        This class provides methods to calculate the average rating, minimum and maximum ratings, 
+        percentages of listings that are above or below the average, minimum, and maximum ratings.
+        It allows initialization with a CSV file from which data is loaded and cleaned,
+        or directly with given pandas DataFrame.
+    """
 
     def __init__(self, csv_path: str = None, df: pd.DataFrame = None):
+        """ Initializes the class with data from a CSV file or an existing DataFrame 
+        Data is either read from the specified CSV file and cleaned, 
+        or DataFrame is provided
+        Args:
+            csv_path (str, optional): Path to the CSV file to be read and cleaned. Defaults to None
+            df (pd.DataFrame, optional): Existing DataFrame to be used directly. Defaults to None
+        Raises:
+            AssertionError: If neither csv_path nor df is provided.
+        """
         assert csv_path is not None or df is not None, "Either csv_path or df must be provided"
         if df is not None:
             self.df = df
@@ -12,20 +29,92 @@ class RatingSummary:
             self.clean_data()
 
     def clean_data(self) -> pd.DataFrame:
-        self.df.dropna(subset=['review_rate_number'], inplace=True)
+        """ Drops NAN values for review rate number 
+        Returns:
+            pd.DataFrame: cleaned dataframe
+        """
+        self.df.dropna(subset=['review rate number'], inplace=True)
         return self.df
 
-    def get_average_rating_per_nhood(self) -> pd.DataFrame:
-        grouped = self.df.groupby('neighbourhood_group')
-        average_ratings = grouped['review_rate_number'].mean()
-        return average_ratings
+    def average_rating(self):
+        """
+        Returns:
+            float: average rating
+        """
+        return round(self.df['review rate number'].mean(),3)
 
-    def get_min_rating_per_nhood(self) -> pd.DataFrame:
-        grouped = self.df.groupby('neighbourhood_group')
-        min_rating = grouped['review_rate_number'].min()
-        return min_rating
+    def percentage_rating_over_average(self):
+        """ 
+        Returns:
+            float: percentage of AirBnB with better rating then average
+        """
+        over_average = self.df[self.df['review rate number'] > self.average_rating()].shape[0]
+        return self.calculate_percentage(over_average)
 
-    def get_max_rating_per_nhood(self) -> pd.DataFrame:
-        grouped = self.df.groupby('neighbourhood group')
-        max_rating = grouped['review_rate_number'].max()
-        return max_rating
+    def percentage_rating_under_average(self):
+        """ 
+        Returns:
+            float: percentage of AirBnB with worse rating then average
+        """
+        under_average = self.df[self.df['review rate number'] < self.average_rating()].shape[0]
+        return self.calculate_percentage(under_average)
+        
+    def min_rating(self):
+        """ 
+        Returns:
+            int: min rating of all AirBnBs
+        """
+        return self.df['review rate number'].min()
+        
+        
+    def max_rating(self):
+        """ 
+        Returns:
+            int: max rating of all AirBnBs
+        """
+        return self.df['review rate number'].max()
+        
+    def percentage_under_min_rating(self):
+        """ 
+        Returns:
+            float: percentage of AirBnB with worse rating then min rating
+        """
+        under_min = self.df[self.df['review rate number'] < self.min_rating()].shape[0]
+        return self.calculate_percentage(under_min)
+    
+    def percentage_over_min_rating(self):
+        """ 
+        Returns:
+            float: percentage of AirBnB with higher rating then min rating
+        """
+        over_min = self.df[self.df['review rate number'] > self.min_rating()].shape[0]
+        return self.calculate_percentage(over_min)
+
+    def percentage_under_max_rating(self):
+        """ 
+        Returns:
+            float: percentage of AirBnB with worse rating then max rating
+        """
+        under_max = self.df[self.df['review rate number'] < self.max_rating()].shape[0]
+        return self.calculate_percentage(under_max)
+    
+    def percentage_over_max_rating(self): 
+        """ 
+        Returns:
+            float: percentage of AirBnB with higher rating then max rating
+        """
+        over_max = self.df[self.df['review rate number'] > self.max_rating()].shape[0]
+        return self.calculate_percentage(over_max)
+
+    def calculate_percentage(self,value):
+        """ Calculate the percentage of a specific subset of Airbnb listings
+        relative to the total number of listings.
+        Args:
+            value (int): The count of Airbnb listings meeting a specific condition.
+        Returns:
+            float: percentage of listings that meet the condition, 
+            rounded to three decimal places.
+        """
+        total = self.df.shape[0]
+        return round(100 * value/total, 3) 
+ 
