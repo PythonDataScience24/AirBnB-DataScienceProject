1cf91b8509322c0ca050ba727687dd899ed5952a
romanobrentani <brentani.v@gmail.com>
2024-05-22 19:06:25 +0200

    added doc string and changed title in plot

--- src/neighbourhhod_summary_plotter.py
+++ src/neighbourhhod_summary_plotter.py
@@ -23,7 +23,7 @@ class NeighbourhoodSummaryPlotter:
         will only be rendered if room type is not selected
         """
         fig, ax = plt.subplots(figsize=(5, 5))
-        plt.title("Total numbers of accommodations by each room type")
+        plt.title("Total numbers of accommodations of each room type")
         room_typed_count = self.df.groupby("room_type")["room_type"].count()
         room_types = room_typed_count.index.values
         counts = room_typed_count.values
@@ -41,7 +41,7 @@ class NeighbourhoodSummaryPlotter:
         room_types_count = self.df.groupby("room_type")["room_type"].count()
         data = room_types_count * 100 / self.df.shape[0]
         labels = room_types_count.index.values
-        ax.set_title("Occurrence percentage of each room type")
-        ax.pie(data, autopct='%1.1f%%')
+        ax.set_title("Occurrence of each room type")
+        ax.pie(data, autopct='%1.1f%%', labels=labels)
         ax.legend(labels, loc='lower left', bbox_to_anchor=(1, 0, 0.5, 1))
         return fig
