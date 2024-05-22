bcc0623cef58c433d613ffc511d91e349f550e4d
Viola Meier <viola.camille.meier@gmail.com>
2024-05-22 17:40:30 +0200

    updated rating_plotter

--- src/rating_plotter.py
+++ src/rating_plotter.py
@@ -1,20 +1,21 @@
-import streamlit as st 
+import pandas as pd
 import matplotlib.pyplot as plt
-
-
+import streamlit as st
+from rating import RatingSummary
+   
 class RatingPlotter:
-    def __init__(self, rating_summary):
-        self.rating_summary = rating_summary
+    def __init__(self, rating_summary: RatingSummary):
+        self.rating_summary: RatingSummary = rating_summary
 
 
         
 
     def plot_rating(self):
-        fig, ax = plt.subplot()
         bins = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5]
-        ax.hist(self.rating_summary.df['review rate number'], bins=bins, rwidth=0.5)
+        plot = plt.hist(self.rating_summary.df['review rate number'], bins = bins, 
+                rwidth = 0.5)
         plt.xticks([1, 2, 3, 4, 5]) 
         plt.title('Rating Distribution of')
         plt.xlabel('Rating')
         plt.ylabel('Number of AirBnBs')
-        st.pyplot(fig)
+        st.pyplot(plot)
