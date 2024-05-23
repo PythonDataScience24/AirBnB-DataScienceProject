85360f61ec5d17b8479a3e8c89fd22aa74773bd8
romanobrentani <brentani.v@gmail.com>
2024-05-23 11:36:50 +0200

    removed title in legend box

--- src/rating_plotter.py
+++ src/rating_plotter.py
@@ -4,25 +4,23 @@ from rating import RatingSummary
 
 
 class RatingPlotter:
-    
+
     def __init__(self, rating_summary: RatingSummary):
         self.rating_summary: RatingSummary = rating_summary
 
-
-            
     def plot_hist_rating(self):
-        fig,ax = plt.subplots()
+        fig, ax = plt.subplots()
         bins = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5]
         ax.hist(self.rating_summary.df['review_rate_number'], bins=bins,
-                        rwidth=0.5)
+                rwidth=0.5)
         plt.xticks([1, 2, 3, 4, 5])
         plt.title('Rating Distribution')
         plt.xlabel('Rating')
         plt.ylabel('Number of AirBnBs')
         st.pyplot(fig)
-        
-    def plot_pie_average(self): 
-        fig, ax = plt.subplots(figsize=(3,3))
+
+    def plot_pie_average(self):
+        fig, ax = plt.subplots(figsize=(3, 3))
         data = []
         over_avrg = self.rating_summary.percentage_rating_over_average()
         under_avrg = self.rating_summary.percentage_rating_under_average()
@@ -30,5 +28,5 @@ class RatingPlotter:
         data.append(under_avrg)
         labels = ["Rating over Average", 'Rating under Average']
         ax.set_title("Rating")
-        ax.pie(data, labels= labels, autopct = '%1.1f%%')
+        ax.pie(data, labels=labels, autopct='%1.1f%%')
         st.pyplot(fig)
